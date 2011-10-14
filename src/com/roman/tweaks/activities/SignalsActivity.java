d5269e6f3d19b34ce693f6e0e15a96d6ebe7400d
Derek Reynolds <myn_2000@yahoo.com>
2011-10-13 21:35:35 -0400

    Added 6 Signal Bar Support (Not wired up to SystemUI Yet though)

--- src/com/roman/tweaks/activities/SignalsActivity.java
+++ src/com/roman/tweaks/activities/SignalsActivity.java
@@ -1,4 +1,3 @@
-
 package com.roman.tweaks.activities;
 
 import com.roman.tweaks.R;
@@ -14,200 +13,236 @@ import android.preference.PreferenceScreen;
 import android.provider.Settings;
 import android.util.Log;
 
-public class SignalsActivity extends PreferenceActivity implements OnPreferenceChangeListener {
+public class SignalsActivity extends PreferenceActivity implements
+		OnPreferenceChangeListener {
 
-    private String signalTextColorPickerFlag;
+	private String signalTextColorPickerFlag;
 
-    // color preference constants
-    private static final String PREF_SIGNAL_COLOR_0 = "signal_color_0";
-    private static final String PREF_SIGNAL_COLOR_1 = "signal_color_1";
-    private static final String PREF_SIGNAL_COLOR_2 = "signal_color_2";
-    private static final String PREF_SIGNAL_COLOR_3 = "signal_color_3";
-    private static final String PREF_SIGNAL_COLOR_4 = "signal_color_4";
-    private static final String PREF_SIGNAL_COLOR_STATIC = "signal_color_static";
-    private static final String PREF_AUTO_COLOR = "signal_automatically_color_pref";
-    private static final String PREF_SIGNAL_TEXT_STYLE = "signal_text_style_pref";
-    //private static final String PREF_TOGGLE_4G_ICON = "show_4g_icon";
-    //private static final String PREF_TOGGLE_2G_ICON = "show_2g_icon";
-    private static final String PREF_SHOW_SIGNAL_BARS = "show_signal_bars";
+	// color preference constants
+	private static final String PREF_SIGNAL_COLOR_0 = "signal_color_0";
+	private static final String PREF_SIGNAL_COLOR_1 = "signal_color_1";
+	private static final String PREF_SIGNAL_COLOR_2 = "signal_color_2";
+	private static final String PREF_SIGNAL_COLOR_3 = "signal_color_3";
+	private static final String PREF_SIGNAL_COLOR_4 = "signal_color_4";
+	private static final String PREF_SIGNAL_COLOR_5 = "signal_color_5";
+	private static final String PREF_SIGNAL_COLOR_6 = "signal_color_6";
+	private static final String PREF_SIGNAL_COLOR_STATIC = "signal_color_static";
+	private static final String PREF_AUTO_COLOR = "signal_automatically_color_pref";
+	private static final String PREF_SIGNAL_TEXT_STYLE = "signal_text_style_pref";
+	// private static final String PREF_TOGGLE_4G_ICON = "show_4g_icon";
+	// private static final String PREF_TOGGLE_2G_ICON = "show_2g_icon";
+	private static final String PREF_SHOW_SIGNAL_BARS = "show_signal_bars";
 
-    //
-    Preference mSignalColor0;
-    Preference mSignalColor1;
-    Preference mSignalColor2;
-    Preference mSignalColor3;
-    Preference mSignalColor4;
-    Preference mSignalColorStatic;
-    CheckBoxPreference mSignalAutoColor;
-    CheckBoxPreference mShow4GIcon;
-    CheckBoxPreference mShow2GIcon;
-    CheckBoxPreference mShowSignalBars;
-    ListPreference mSignalTextStyle;
+	//
+	Preference mSignalColor0;
+	Preference mSignalColor1;
+	Preference mSignalColor2;
+	Preference mSignalColor3;
+	Preference mSignalColor4;
+	Preference mSignalColor5;
+	Preference mSignalColor6;
+	Preference mSignalColorStatic;
+	CheckBoxPreference mSignalAutoColor;
+	CheckBoxPreference mShow4GIcon;
+	CheckBoxPreference mShow2GIcon;
+	CheckBoxPreference mShowSignalBars;
+	ListPreference mSignalTextStyle;
 
-    public void onCreate(Bundle ofLove) {
-        super.onCreate(ofLove);
-        addPreferencesFromResource(R.xml.signal_prefs);
+	public void onCreate(Bundle ofLove) {
+		super.onCreate(ofLove);
+		addPreferencesFromResource(R.xml.signal_prefs);
 
-        // assign
-        PreferenceScreen prefs = getPreferenceScreen();
-        mSignalAutoColor = (CheckBoxPreference) prefs.findPreference(PREF_AUTO_COLOR);
-        mSignalTextStyle = (ListPreference) prefs.findPreference(PREF_SIGNAL_TEXT_STYLE);
-        mSignalColor0 = prefs.findPreference(PREF_SIGNAL_COLOR_0);
-        mSignalColor1 = prefs.findPreference(PREF_SIGNAL_COLOR_1);
-        mSignalColor2 = prefs.findPreference(PREF_SIGNAL_COLOR_2);
-        mSignalColor3 = prefs.findPreference(PREF_SIGNAL_COLOR_3);
-        mSignalColor4 = prefs.findPreference(PREF_SIGNAL_COLOR_4);
-        mSignalColorStatic = prefs.findPreference(PREF_SIGNAL_COLOR_STATIC);
-        // mShow4GIcon = (CheckBoxPreference) prefs.findPreference(PREF_TOGGLE_4G_ICON);
-        // mShow2GIcon = (CheckBoxPreference) prefs.findPreference(PREF_TOGGLE_2G_ICON);
-        mShowSignalBars = (CheckBoxPreference) prefs.findPreference(PREF_SHOW_SIGNAL_BARS);
+		// assign
+		PreferenceScreen prefs = getPreferenceScreen();
+		mSignalAutoColor = (CheckBoxPreference) prefs
+				.findPreference(PREF_AUTO_COLOR);
+		mSignalTextStyle = (ListPreference) prefs
+				.findPreference(PREF_SIGNAL_TEXT_STYLE);
+		mSignalColor0 = prefs.findPreference(PREF_SIGNAL_COLOR_0);
+		mSignalColor1 = prefs.findPreference(PREF_SIGNAL_COLOR_1);
+		mSignalColor2 = prefs.findPreference(PREF_SIGNAL_COLOR_2);
+		mSignalColor3 = prefs.findPreference(PREF_SIGNAL_COLOR_3);
+		mSignalColor4 = prefs.findPreference(PREF_SIGNAL_COLOR_4);
+		mSignalColor5 = prefs.findPreference(PREF_SIGNAL_COLOR_5);
+		mSignalColor6 = prefs.findPreference(PREF_SIGNAL_COLOR_6);
+		mSignalColorStatic = prefs.findPreference(PREF_SIGNAL_COLOR_STATIC);
+		// mShow4GIcon = (CheckBoxPreference)
+		// prefs.findPreference(PREF_TOGGLE_4G_ICON);
+		// mShow2GIcon = (CheckBoxPreference)
+		// prefs.findPreference(PREF_TOGGLE_2G_ICON);
+		mShowSignalBars = (CheckBoxPreference) prefs
+				.findPreference(PREF_SHOW_SIGNAL_BARS);
 
-        
-        
-        
-        // check enabled settings
-        mShowSignalBars.setChecked((Settings.System.getInt(getContentResolver(),
-                "tweaks_signal_icon_enabled", 1) == 1));
-        mSignalAutoColor.setChecked((Settings.System.getInt(getContentResolver(),
-                "tweaks_signal_text_autocolor_enabled", 0) == 1));
-        /* mShow4GIcon.setChecked((Settings.System.getInt(getContentResolver(),
-                "tweaks_show_4g_icon", 0) == 1)); */
-        /* mShow2GIcon.setChecked((Settings.System.getInt(getContentResolver(),
-                "tweaks_show_2g_icon", 0) == 1)); */
+		// check enabled settings
+		mShowSignalBars.setChecked((Settings.System.getInt(
+				getContentResolver(), "tweaks_signal_icon_enabled", 1) == 1));
+		mSignalAutoColor.setChecked((Settings.System
+				.getInt(getContentResolver(),
+						"tweaks_signal_text_autocolor_enabled", 0) == 1));
+		/*
+		 * mShow4GIcon.setChecked((Settings.System.getInt(getContentResolver(),
+		 * "tweaks_show_4g_icon", 0) == 1));
+		 */
+		/*
+		 * mShow2GIcon.setChecked((Settings.System.getInt(getContentResolver(),
+		 * "tweaks_show_2g_icon", 0) == 1));
+		 */
 
-        mSignalTextStyle.setOnPreferenceChangeListener(this);
-        mSignalTextStyle.setValueIndex(Settings.System.getInt(getContentResolver(),
-                "tweaks_signal_text_style", 0));
+		mSignalTextStyle.setOnPreferenceChangeListener(this);
+		mSignalTextStyle.setValueIndex(Settings.System.getInt(
+				getContentResolver(), "tweaks_signal_text_style", 0));
 
-        refreshOptions();
-    }
+		refreshOptions();
+	}
 
-    public void onResume(Bundle ofLove) {
-        refreshOptions();
-    }
+	public void onResume(Bundle ofLove) {
+		refreshOptions();
+	}
 
-    public ColorPickerDialog generateDialog(String preference) {
-        ColorPickerDialog cp = new ColorPickerDialog(this, mColorChangeListener,
-                Settings.System.getInt(getContentResolver(), preference,
-                        Settings.System.getInt(getContentResolver(), preference, Color.WHITE)));
-        return cp;
-    }
+	public ColorPickerDialog generateDialog(String preference) {
+		ColorPickerDialog cp = new ColorPickerDialog(this,
+				mColorChangeListener, Settings.System.getInt(
+						getContentResolver(), preference, Settings.System
+								.getInt(getContentResolver(), preference,
+										Color.WHITE)));
+		return cp;
+	}
 
-    public boolean onPreferenceTreeClick(PreferenceScreen screen, Preference preference) {
+	public boolean onPreferenceTreeClick(PreferenceScreen screen,
+			Preference preference) {
 
-        if (preference == mSignalColor0) {
+		if (preference == mSignalColor0) {
 
-            signalTextColorPickerFlag = "tweaks_signal_text_color_0_bar";
+			signalTextColorPickerFlag = "tweaks_signal_text_color_0_bar";
 
-            generateDialog(signalTextColorPickerFlag).show();
-            return true;
+			generateDialog(signalTextColorPickerFlag).show();
+			return true;
 
-        } else if (preference == mSignalColor1) {
-            signalTextColorPickerFlag = "tweaks_signal_text_color_1_bar";
+		} else if (preference == mSignalColor1) {
+			signalTextColorPickerFlag = "tweaks_signal_text_color_1_bar";
 
-            generateDialog(signalTextColorPickerFlag).show();
-            return true;
+			generateDialog(signalTextColorPickerFlag).show();
+			return true;
 
-        } else if (preference == mSignalColor2) {
+		} else if (preference == mSignalColor2) {
 
-            signalTextColorPickerFlag = "tweaks_signal_text_color_2_bar";
-            generateDialog(signalTextColorPickerFlag).show();
-            return true;
+			signalTextColorPickerFlag = "tweaks_signal_text_color_2_bar";
+			generateDialog(signalTextColorPickerFlag).show();
+			return true;
 
-        } else if (preference == mSignalColor3) {
+		} else if (preference == mSignalColor3) {
 
-            signalTextColorPickerFlag = "tweaks_signal_text_color_3_bar";
-            generateDialog(signalTextColorPickerFlag).show();
-            return true;
+			signalTextColorPickerFlag = "tweaks_signal_text_color_3_bar";
+			generateDialog(signalTextColorPickerFlag).show();
+			return true;
 
-        } else if (preference == mSignalColor4) {
+		} else if (preference == mSignalColor4) {
 
-            signalTextColorPickerFlag = "tweaks_signal_text_color_4_bar";
-            generateDialog(signalTextColorPickerFlag).show();
-            return true;
+			signalTextColorPickerFlag = "tweaks_signal_text_color_4_bar";
+			generateDialog(signalTextColorPickerFlag).show();
+			return true;
 
-        } else if (preference == mSignalColorStatic) {
+		} else if (preference == mSignalColor5) {
 
-            signalTextColorPickerFlag = "tweaks_signal_text_color_static";
-            generateDialog(signalTextColorPickerFlag).show();
-            return true;
+			signalTextColorPickerFlag = "tweaks_signal_text_color_5_bar";
+			generateDialog(signalTextColorPickerFlag).show();
+			return true;
 
-        } else if (preference == mSignalAutoColor) {
-            boolean enable = mSignalAutoColor.isChecked();
-            Settings.System.putInt(getContentResolver(),
-                    "tweaks_signal_text_autocolor_enabled", (enable ? 1 : 0));
-            refreshOptions();
-            return true;
+		} else if (preference == mSignalColor6) {
 
-        } else if (preference == mShow4GIcon) {
-            boolean enable = mShow4GIcon.isChecked();
-            Settings.System.putInt(getContentResolver(),
-                    "tweaks_show_4g_icon", (enable ? 1 : 0));
-            return true;
+			signalTextColorPickerFlag = "tweaks_signal_text_color_6_bar";
+			generateDialog(signalTextColorPickerFlag).show();
+			return true;
 
-        } else if (preference == mShow2GIcon) {
-            boolean enable = ((CheckBoxPreference) preference).isChecked();
+		} else if (preference == mSignalColorStatic) {
 
-            Log.e("Roman", "2G toggle");
-            Settings.System.putInt(getContentResolver(),
-                    "tweaks_show_2g_icon", (enable ? 1 : 0));
-            return true;
+			signalTextColorPickerFlag = "tweaks_signal_text_color_static";
+			generateDialog(signalTextColorPickerFlag).show();
+			return true;
 
-        } else if (preference == mShowSignalBars) {
-            boolean enable = mShowSignalBars.isChecked();
-            Settings.System.putInt(getContentResolver(),
-                    "tweaks_signal_icon_enabled", (enable ? 1 : 0));
+		} else if (preference == mSignalAutoColor) {
+			boolean enable = mSignalAutoColor.isChecked();
+			Settings.System.putInt(getContentResolver(),
+					"tweaks_signal_text_autocolor_enabled", (enable ? 1 : 0));
+			refreshOptions();
+			return true;
 
-            return true;
+		} else if (preference == mShow4GIcon) {
+			boolean enable = mShow4GIcon.isChecked();
+			Settings.System.putInt(getContentResolver(), "tweaks_show_4g_icon",
+					(enable ? 1 : 0));
+			return true;
 
-        } else
+		} else if (preference == mShow2GIcon) {
+			boolean enable = ((CheckBoxPreference) preference).isChecked();
 
-            return false;
-    }
+			Log.e("Roman", "2G toggle");
+			Settings.System.putInt(getContentResolver(), "tweaks_show_2g_icon",
+					(enable ? 1 : 0));
+			return true; 
 
-    public boolean onPreferenceChange(Preference preference, Object newValue) {
-        Log.d("TWEAKS", preference.getKey());
+		} else if (preference == mShowSignalBars) {
+			boolean enable = mShowSignalBars.isChecked();
+			Settings.System.putInt(getContentResolver(),
+					"tweaks_signal_icon_enabled", (enable ? 1 : 0));
 
-        if (preference == mSignalTextStyle) {
-            preference = (ListPreference) preference;
+			return true;
 
-            int val = Integer.valueOf((String) newValue);
-            Settings.System.putInt(getContentResolver(), "tweaks_signal_text_style",
-                    val);
-            return true;
-        }
+		} else
 
-        return false;
-    }
+			return false;
+	}
 
-    ColorPickerDialog.OnColorChangedListener mColorChangeListener = new ColorPickerDialog.OnColorChangedListener() {
+	public boolean onPreferenceChange(Preference preference, Object newValue) {
+		Log.d("TWEAKS", preference.getKey());
 
-        public void colorUpdate(int color) {
-            Settings.System.putInt(getContentResolver(), signalTextColorPickerFlag, color);
-        }
+		if (preference == mSignalTextStyle) {
+			preference = (ListPreference) preference;
 
-        public void colorChanged(int color) {
-            Settings.System.putInt(getContentResolver(), signalTextColorPickerFlag, color);
+			int val = Integer.valueOf((String) newValue);
+			Settings.System.putInt(getContentResolver(),
+					"tweaks_signal_text_style", val);
+			return true;
+		}
 
-        }
-    };
+		return false;
+	}
 
-    public void refreshOptions() {
-        if (((CheckBoxPreference) findPreference("signal_automatically_color_pref")).isChecked()) {
-            findPreference("signal_color_static").setEnabled(false);
-            findPreference("signal_color_0").setEnabled(true);
-            findPreference("signal_color_1").setEnabled(true);
-            findPreference("signal_color_2").setEnabled(true);
-            findPreference("signal_color_3").setEnabled(true);
-            findPreference("signal_color_4").setEnabled(true);
-        } else {
-            findPreference("signal_color_static").setEnabled(true);
-            findPreference("signal_color_0").setEnabled(false);
-            findPreference("signal_color_1").setEnabled(false);
-            findPreference("signal_color_2").setEnabled(false);
-            findPreference("signal_color_3").setEnabled(false);
-            findPreference("signal_color_4").setEnabled(false);
-        }
-    }
+	ColorPickerDialog.OnColorChangedListener mColorChangeListener = new ColorPickerDialog.OnColorChangedListener() {
+
+		public void colorUpdate(int color) {
+			Settings.System.putInt(getContentResolver(),
+					signalTextColorPickerFlag, color);
+		}
+
+		public void colorChanged(int color) {
+			Settings.System.putInt(getContentResolver(),
+					signalTextColorPickerFlag, color);
+
+		}
+	};
+
+	public void refreshOptions() {
+		if (((CheckBoxPreference) findPreference("signal_automatically_color_pref"))
+				.isChecked()) {
+			findPreference("signal_color_static").setEnabled(false);
+			findPreference("signal_color_0").setEnabled(true);
+			findPreference("signal_color_1").setEnabled(true);
+			findPreference("signal_color_2").setEnabled(true);
+			findPreference("signal_color_3").setEnabled(true);
+			findPreference("signal_color_4").setEnabled(true);
+			findPreference("signal_color_5").setEnabled(true);
+			findPreference("signal_color_6").setEnabled(true);
+		} else {
+			findPreference("signal_color_static").setEnabled(true);
+			findPreference("signal_color_0").setEnabled(false);
+			findPreference("signal_color_1").setEnabled(false);
+			findPreference("signal_color_2").setEnabled(false);
+			findPreference("signal_color_3").setEnabled(false);
+			findPreference("signal_color_4").setEnabled(false);
+			findPreference("signal_color_5").setEnabled(false);
+			findPreference("signal_color_6").setEnabled(false);
+		}
+	}
 
 }
